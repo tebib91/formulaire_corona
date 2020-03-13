d163c571fb2be937f280092cc79c1c785900113c
Tebib Ahmed <tebib.ahmed0@gmail.com>
2020-03-13 18:17:50 +0100

    layout basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  {path: '' , redirectTo:'dashboard',pathMatch: 'full'},
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
 ];
