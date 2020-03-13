9c123ccbb5e5e4029071244ae195ba182b8f3e84
mourad <mourad.riahi@vynd.com>
2020-03-13 19:38:40 +0100

    Form && auto question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FormsComponent} from './home/forms/forms.component';
+import {AutoQuestionsComponent} from './home/forms/auto-questions/auto-questions.component';
 
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+  {
+    path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'firstForm', component: FormsComponent
+  },
+  {
+    path: 'secondForm', component: AutoQuestionsComponent
+  }
 ];
 
 @NgModule({
