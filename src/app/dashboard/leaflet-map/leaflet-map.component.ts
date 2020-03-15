666932a142162e4b6ba5488dab10d6c4f37e0d75
Dhia Eddine <dhiaeddine214@gmail.com>
2020-03-15 20:04:34 +0100

    wired new map

--- src/app/dashboard/leaflet-map/leaflet-map.component.ts
+++ src/app/dashboard/leaflet-map/leaflet-map.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, AfterViewInit } from "@angular/core";
+import * as L from "leaflet";
 @Component({
-  selector: 'app-leaflet-map',
-  templateUrl: './leaflet-map.component.html',
-  styleUrls: ['./leaflet-map.component.scss']
+  selector: "app-leaflet-map",
+  templateUrl: "./leaflet-map.component.html",
+  styleUrls: ["./leaflet-map.component.scss"]
 })
-export class LeafletMapComponent implements OnInit {
+export class LeafletMapComponent implements OnInit, AfterViewInit {
+  @Input() dataSource: string;
+  private map;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ngAfterViewInit() {
+    this.initMap();
   }
+  private initMap(): void {
+    this.map = L.map("map", {
+      center: [33.8869, 9.5375],
+      zoom: 6
+    });
+    const tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    );
 
+    tiles.addTo(this.map);
+    const marker = L.marker([32.9210902, 10.4508956]);
+    marker.addTo(this.map);
+  }
 }
