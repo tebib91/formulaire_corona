87ba6ebb67566251d49ef077a49b1561a4a5123e
mourad <mourad.riahi@vynd.com>
2020-03-21 15:25:53 +0100

    declaration add preaumbule

--- src/app/home/forms/auto-questions/auto-questions.component.ts
+++ src/app/home/forms/auto-questions/auto-questions.component.ts
@@ -25,9 +25,10 @@ export class AutoQuestionsComponent implements OnInit {
   testingIndex = 0;
   specimensIndex = 0;
   show = false;
-  toDisplay = 1;
+  toDisplay = 0;
   done = false;
   index = 0;
+  length = 0;
 
   constructor(private fb: FormBuilder, private api: ApiserviceService, private snackBar: MatSnackBar) {
   }
@@ -79,6 +80,7 @@ export class AutoQuestionsComponent implements OnInit {
       }
     });
     this.specimensForm = this.fb.group(specimen);
+    this.length = this.symptomValues.length;
   }
 
 
@@ -92,19 +94,16 @@ export class AutoQuestionsComponent implements OnInit {
     let hasError = false;
     if (this.toDisplay === 1) {
       index = 'symptomsIndex';
-      length = this.symptomValues.length;
+      this.length = this.symptomValues.length;
       hasError = this.symptomForm.controls[this.symptomValues[this[index]].value].valid;
     } else if (this.toDisplay === 2) {
       index = 'medicalIndex';
       hasError = this.medicalForm.controls[this.medicalExtension[this[index]].value].valid;
-      // console.log('object :', this.medicalForm.controls[this.medicalExtension[this[index]].value].valid);
-      // console.log('valid :', this.medicalForm.get[this.medicalExtension[this[index]].value].valid);
-      // console.log('validator :', this.medicalForm.controls[this.medicalExtension[this[index]].value]);
-      length = this.medicalExtension.length;
+      this.length = this.medicalExtension.length;
     } else if (this.toDisplay === 3) {
       index = 'testingIndex';
       hasError = this.testingForm.controls[this.testingDiag[this[index]].value].valid;
-      length = this.testingDiag.length;
+      this.length = this.testingDiag.length;
     }
     if (!hasError) {
       this.openSnackBar();
@@ -112,7 +111,7 @@ export class AutoQuestionsComponent implements OnInit {
     setTimeout(() => {
       console.log(index + ':', this[index], 'To Display :', this.toDisplay);
       console.log('hasError :', hasError);
-      if ((this[index] < length - 1) && hasError === true) {
+      if ((this[index] < this.length - 1) && hasError === true) {
         this[index]++;
         this.index = this[index];
       } else {
@@ -121,7 +120,7 @@ export class AutoQuestionsComponent implements OnInit {
           this.index = 0;
         }
       }
-      if (this.toDisplay && this.testingIndex === length - 1) {
+      if (this.toDisplay && this.testingIndex === this.length - 1) {
         this.done = true;
 
       }
@@ -142,7 +141,7 @@ export class AutoQuestionsComponent implements OnInit {
       this[index]--;
       this.index = this[index];
     } else {
-      if (this.toDisplay !== 1) {
+      if (this.toDisplay !== 0) {
         this.toDisplay--;
         this.index = 0;
       }
@@ -177,5 +176,9 @@ export class AutoQuestionsComponent implements OnInit {
       sentCDC: false,
       CDCResult: ['', Validators.required]
     });
+  }
+
+  gotToTest() {
+    this.toDisplay++;
   }
 }
